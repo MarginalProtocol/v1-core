afd845a819a50708d4d2968fff44452a1ddf6dc3
0xcivita <0xcivita@proton.me>
2023-05-14 11:53:04 -0400

    solidity linting

--- contracts/MarginalV1Factory.sol
+++ contracts/MarginalV1Factory.sol
@@ -6,20 +6,31 @@ import {MarginalV1Pool} from "./MarginalV1Pool.sol";
 contract MarginalV1Factory {
     mapping(address => mapping(address => mapping(uint256 => address))) getPool;
 
-    event PoolCreated(address token0, address token1, uint256 maintenance, address pool);
+    event PoolCreated(
+        address token0,
+        address token1,
+        uint256 maintenance,
+        address pool
+    );
 
     constructor() {}
-    
+
     function createPool(
         address tokenA,
         address tokenB,
         uint256 maintenance // TODO: smaller type
     ) external returns (address pool) {
         require(tokenA != tokenB, "A == B");
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        (address token0, address token1) = tokenA < tokenB
+            ? (tokenA, tokenB)
+            : (tokenB, tokenA);
         require(token0 != address(0));
 
-        pool = address(new MarginalV1Pool{salt: keccak256(abi.encode(token0, token1, maintenance))}());
+        pool = address(
+            new MarginalV1Pool{
+                salt: keccak256(abi.encode(token0, token1, maintenance))
+            }()
+        );
 
         // populate in reverse for key (token0, token1, maintenance)
         getPool[token0][token1][maintenance] = pool;
