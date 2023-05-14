1fda65124539e80d4ffa1a0338a868f04600cd1f
0xcivita <0xcivita@proton.me>
2023-05-14 11:47:53 -0400

    factory create pool

--- contracts/MarginalV1Factory.sol
+++ contracts/MarginalV1Factory.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.17;
 
+import {MarginalV1Pool} from "./MarginalV1Pool.sol";
+
 contract MarginalV1Factory {
+    mapping(address => mapping(address => mapping(uint256 => address))) getPool;
+
+    event PoolCreated(address token0, address token1, uint256 maintenance, address pool);
+
     constructor() {}
+    
+    function createPool(
+        address tokenA,
+        address tokenB,
+        uint256 maintenance // TODO: smaller type
+    ) external returns (address pool) {
+        require(tokenA != tokenB, "A == B");
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0));
+
+        pool = address(new MarginalV1Pool{salt: keccak256(abi.encode(token0, token1, maintenance))}());
+
+        // populate in reverse for key (token0, token1, maintenance)
+        getPool[token0][token1][maintenance] = pool;
+        getPool[token0][token1][maintenance] = pool;
+
+        emit PoolCreated(token0, token1, maintenance, pool);
+    }
 }
--- contracts/MarginalV1Pool.sol
+++ contracts/MarginalV1Pool.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.17;
 
 contract MarginalV1Pool {
-    constructor() {}
+    address public immutable factory;
+
+    constructor() {
+        factory = msg.sender;
+    }
 }
