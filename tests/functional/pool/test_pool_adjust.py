ed851015eb28db9bce2a752288bbed770c7cf970
0xcivita <0xcivita@proton.me>
2023-05-29 17:47:50 -0400

    test pool adjust emits event

--- tests/functional/pool/test_pool_adjust.py
+++ tests/functional/pool/test_pool_adjust.py
@@ -101,6 +101,74 @@ def test_pool_adjust__sets_position_with_one_for_zero(
     assert pool_initialized_with_liquidity.positions(key) == position
 
 
+def test_pool_adjust__transfers_funds_with_zero_for_one(
+    pool_initialized_with_liquidity,
+    callee,
+    sender,
+    token0,
+    token1,
+    zero_for_one_position_id,
+):
+    key = get_position_key(sender.address, zero_for_one_position_id)
+    position = pool_initialized_with_liquidity.positions(key)
+
+    margin_in = 2 * position.margin
+    margin_out = position.margin
+    data = encode(["address"], [sender.address])
+
+    balance1_sender = token1.balanceOf(sender.address)
+    balance1_pool = token1.balanceOf(pool_initialized_with_liquidity.address)
+
+    pool_initialized_with_liquidity.adjust(
+        callee.address,
+        zero_for_one_position_id,
+        margin_in,
+        margin_out,
+        data,
+        sender=sender,
+    )
+    assert token1.balanceOf(
+        pool_initialized_with_liquidity.address
+    ) == balance1_pool + (margin_in - margin_out)
+    assert token1.balanceOf(sender.address) == balance1_sender - (
+        margin_in - margin_out
+    )
+
+
+def test_pool_adjust__transfers_funds_with_one_for_zero(
+    pool_initialized_with_liquidity,
+    callee,
+    sender,
+    token0,
+    token1,
+    one_for_zero_position_id,
+):
+    key = get_position_key(sender.address, one_for_zero_position_id)
+    position = pool_initialized_with_liquidity.positions(key)
+
+    margin_in = 2 * position.margin
+    margin_out = position.margin
+    data = encode(["address"], [sender.address])
+
+    balance0_sender = token0.balanceOf(sender.address)
+    balance0_pool = token0.balanceOf(pool_initialized_with_liquidity.address)
+
+    pool_initialized_with_liquidity.adjust(
+        callee.address,
+        one_for_zero_position_id,
+        margin_in,
+        margin_out,
+        data,
+        sender=sender,
+    )
+    assert token0.balanceOf(
+        pool_initialized_with_liquidity.address
+    ) == balance0_pool + (margin_in - margin_out)
+    assert token0.balanceOf(sender.address) == balance0_sender - (
+        margin_in - margin_out
+    )
+
+
 def test_pool_adjust__transfers_funds_when_add_margin_with_zero_for_one(
     pool_initialized_with_liquidity,
     callee,
@@ -253,6 +321,68 @@ def test_pool_adjust__transfers_funds_when_remove_margin_with_one_for_zero(
         == balance0_pool - margin_out
     )
     assert token0.balanceOf(sender.address) == balance0_sender + margin_out
+
+
+def test_pool_adjust__emits_adjust_with_zero_for_one(
+    pool_initialized_with_liquidity,
+    callee,
+    sender,
+    token0,
+    token1,
+    zero_for_one_position_id,
+):
+    key = get_position_key(sender.address, zero_for_one_position_id)
+    position = pool_initialized_with_liquidity.positions(key)
+    margin_in = position.margin // 2
+
+    data = encode(["address"], [sender.address])
+    tx = pool_initialized_with_liquidity.adjust(
+        callee.address,
+        zero_for_one_position_id,
+        margin_in,
+        0,
+        data,
+        sender=sender,
+    )
+    events = tx.decode_logs(pool_initialized_with_liquidity.Adjust)
+    assert len(events) == 1
+    event = events[0]
+
+    assert event.owner == sender.address
+    assert event.id == zero_for_one_position_id
+    assert event.recipient == callee.address
+    assert event.marginAfter == position.margin + margin_in
+
+
+def test_pool_adjust__emits_adjust_with_one_for_zero(
+    pool_initialized_with_liquidity,
+    callee,
+    sender,
+    token0,
+    token1,
+    one_for_zero_position_id,
+):
+    key = get_position_key(sender.address, one_for_zero_position_id)
+    position = pool_initialized_with_liquidity.positions(key)
+    margin_in = position.margin // 2
+
+    data = encode(["address"], [sender.address])
+    tx = pool_initialized_with_liquidity.adjust(
+        callee.address,
+        one_for_zero_position_id,
+        margin_in,
+        0,
+        data,
+        sender=sender,
+    )
+    events = tx.decode_logs(pool_initialized_with_liquidity.Adjust)
+    assert len(events) == 1
+    event = events[0]
+
+    assert event.owner == sender.address
+    assert event.id == one_for_zero_position_id
+    assert event.recipient == callee.address
+    assert event.marginAfter == position.margin + margin_in
 
 
 # TODO: test when not position owner
