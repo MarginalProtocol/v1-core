c64310b304bdc85dcebe66da2561bce362c0eda2
0xcivita <0xcivita@proton.me>
2023-06-05 21:54:31 -0400

    test position lib settle

--- tests/functional/libraries/test_position_settle.py
+++ tests/functional/libraries/test_position_settle.py
@@ -1,2 +1,18 @@
 def test_position_settle(position_lib):
-    pass
+    position = (1000000, 100, 200, 50, 60, True, False, 10000, -100, 200000, 50000)
+    result = position_lib.settle(position)
+
+    position_after = (
+        0,
+        0,
+        0,
+        0,
+        0,
+        position[5],
+        position[6],
+        position[7],
+        position[8],
+        0,
+        0,
+    )
+    assert result == position_after
