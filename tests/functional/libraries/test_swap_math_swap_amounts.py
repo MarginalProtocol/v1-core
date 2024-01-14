c8a7e4f04233a8dce19966acec2b14ceefdc2123
0xcivita <0xcivita@proton.me>
2024-01-14 11:31:45 -0500

    fix swap amounts with fuzz test

--- tests/functional/libraries/test_swap_math_swap_amounts.py
+++ tests/functional/libraries/test_swap_math_swap_amounts.py
@@ -1,10 +1,10 @@
 import pytest
 
-from hypothesis import given
-from hypothesis import strategies as st
+from hypothesis import given, settings, strategies as st
+from datetime import timedelta
 from math import sqrt
 
-from utils.constants import MIN_SQRT_RATIO, MAX_SQRT_RATIO
+from utils.constants import MIN_SQRT_RATIO, MAX_SQRT_RATIO, MINIMUM_LIQUIDITY
 from utils.utils import calc_swap_amounts
 
 
@@ -103,17 +103,20 @@ def test_swap_math_swap_amounts__with_exact_output_one_for_zero(
 
 
 @pytest.mark.fuzzing
+@settings(deadline=timedelta(milliseconds=1000), max_examples=10000)
 @given(
-    liquidity=st.integers(min_value=1000, max_value=2**128 - 1),
-    sqrt_price_x96=st.integers(min_value=MIN_SQRT_RATIO, max_value=MAX_SQRT_RATIO),
-    sqrt_price_x96_next=st.integers(min_value=MIN_SQRT_RATIO, max_value=MAX_SQRT_RATIO),
+    liquidity=st.integers(min_value=MINIMUM_LIQUIDITY, max_value=2**128 - 1),
+    sqrt_price_x96=st.integers(min_value=MIN_SQRT_RATIO, max_value=MAX_SQRT_RATIO - 1),
+    sqrt_price_x96_next=st.integers(
+        min_value=MIN_SQRT_RATIO, max_value=MAX_SQRT_RATIO - 1
+    ),
 )
 def test_swap_math_swap_amounts__with_fuzz(
     swap_math_lib, liquidity, sqrt_price_x96, sqrt_price_x96_next
 ):
-    result = swap_math_lib.swapAmounts(liquidity, sqrt_price_x96, sqrt_price_x96_next)
     (amount0_delta, amount1_delta) = calc_swap_amounts(
         liquidity, sqrt_price_x96, sqrt_price_x96_next
     )
-    assert pytest.approx(result[0], rel=1e-15) == amount0_delta
-    assert pytest.approx(result[1], rel=1e-15) == amount1_delta
+    result = swap_math_lib.swapAmounts(liquidity, sqrt_price_x96, sqrt_price_x96_next)
+    assert pytest.approx(result[0], rel=1e-15, abs=1) == amount0_delta
+    assert pytest.approx(result[1], rel=1e-15, abs=1) == amount1_delta
